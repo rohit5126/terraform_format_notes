4e6c4f91b57127d24ec7e00249aeb777bb39e4d4
rohit5126 <rohit5126kumar@gmail.com>
2026-08-12 18:45:26 +0530

    Update s3.tf

--- EKS-S3/s3.tf
+++ EKS-S3/s3.tf
@@ -1,3 +1,21 @@
+
+
+terraform {
+  required_providers {
+    aws = {
+        source = "hashicorp/aws"
+        version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+    region = "eu-north-1"
+}
+
+# ---------------------------------------------------------------------------
+
+
 resource "aws_s3_bucket" "tf-state" {
     bucket = "diksha-state-bucket-2003"
 
