f57c11b750605b508baabcbea5659e5bb2e9a9da
rohit5126 <rohit5126kumar@gmail.com>
2026-08-12 18:54:47 +0530

    Update terraform.tf

--- EKS-Cluster/terraform.tf
+++ EKS-Cluster/terraform.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
 
     # helm 3.x uses attribute syntax; kubernetes is pinned to v2, which still uses blocks.
