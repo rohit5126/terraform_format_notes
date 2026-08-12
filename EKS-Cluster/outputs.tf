578f199e3ad49ecd3034ed2fb2c5dbd9cbd3215d
rohit5126 <rohit5126kumar@gmail.com>
2026-08-12 17:02:36 +0530

    Update outputs.tf

--- EKS-Cluster/outputs.tf
+++ EKS-Cluster/outputs.tf
@@ -12,8 +12,13 @@ output "cluster_security_group_id" {
   
 }
 
-output "grafana-password" {
-    value = random_password.grafana_admin.result
-    sensitive = true
+output "configure_kubectl" {
+  description = "Point kubectl at the new cluster"
+  value       = "aws eks update-kubeconfig --name ${module.eks.cluster_name} --region ${var.region}"
+}
+
+output "argocd_initial_password" {
+  description = "Read ArgoCD's generated admin password"
+  value       = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d"
 }
 
