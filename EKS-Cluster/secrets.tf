a347e4b35f1ee610c5900850678fefa8f86638e2
rohit5126 <rohit5126kumar@gmail.com>
2026-08-14 00:47:07 +0530

    Update secrets.tf

--- EKS-Cluster/secrets.tf
+++ EKS-Cluster/secrets.tf
@@ -11,7 +11,7 @@ resource "aws_secretsmanager_secret_version" "mysql" {
   secret_id = aws_secretsmanager_secret.mysql.id
   secret_string = jsonencode({
     engine   = "mysql"
-    host     = "a28643631bb424079ab601e44d734fd8-9a26cb8303cd62eb.elb.eu-north-1.amazonaws.com"
+    host     = "acd55e35ea9344c158f02db97576fe25-df46a853f7cecd40.elb.eu-north-1.amazonaws.com"
     port     = 3306
     username = "root"
     password = random_password.mysql_password.result
