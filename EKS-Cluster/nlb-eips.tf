aebf5a274525f0215f2223a5dd8b813fd9bd4a09
rohit5126 <rohit5126kumar@gmail.com>
2026-08-18 17:10:39 +0530

    Create nlb-eips.tf

---
+++ EKS-Cluster/nlb-eips.tf
@@ -0,0 +1,9 @@
+resource "aws_eip" "nlb" {
+  count  = length(module.eks.public_subnets) # match however many AZs your NLB spans
+  domain = "vpc"
+  tags   = { Name = "bankapp-nlb-eip-${count.index}" }
+}
+
+output "nlb_eip_allocation_ids" {
+  value = join(",", aws_eip.nlb[*].id)
+}
